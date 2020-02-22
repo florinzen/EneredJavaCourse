6eef9bef32b648eb50db78bef02bebdd11c73b12
Andrei Herdes <a.herdes@levi9.com>
2020-02-22 19:54:48 +0200

    Added custom exception example

---
+++ SandboxEnered/src/module3/exceptions/customexceptions/PasswordController.java
@@ -0,0 +1,37 @@
+package module3.exceptions.customexceptions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class PasswordController {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduceti parola:");
+        String userPass = in.nextLine();
+
+        try {
+            handlePasswordInput(userPass);
+        } catch(InvalidPasswordException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Un utilizator a accesat functionalitatea de login.");
+        }
+    }
+
+    public static void handlePasswordInput(String input) throws InvalidPasswordException {
+        boolean isPasswordValid = checkPassword(input);
+
+        if(isPasswordValid) {
+            System.out.println("Te-ai logat cu success");
+        } else {
+            throw new InvalidPasswordException("Parola invalida!!!");
+        }
+    }
+
+    public static boolean checkPassword(String password) {
+        List<String> passwords = Arrays.asList("12345", "parolamea");
+        return passwords.contains(password);
+    }
+}
