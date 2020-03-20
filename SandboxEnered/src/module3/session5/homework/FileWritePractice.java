6cb72f7f9df25401347c514535bace5c51ee2314
Andrei Herdes <a.herdes@levi9.com>
2020-03-20 18:59:27 +0200

    Added Session 5 homework solution

---
+++ SandboxEnered/src/module3/session5/homework/FileWritePractice.java
@@ -0,0 +1,33 @@
+package module3.session5.homework;
+
+import java.io.*;
+
+public class FileWritePractice {
+
+    public static void main(String[] args) {
+        try (BufferedWriter bufOutput = new BufferedWriter(new FileWriter("input-from-user.txt"));
+             BufferedReader bufInput = new BufferedReader(new InputStreamReader(System.in))) {
+            //Scanner input = new Scanner(System.in);
+
+            String userInput;
+            do {
+                // scrie inputul utilizatorului intr-un fisier
+                System.out.println("Enter something: ");
+                userInput = bufInput.readLine();
+
+                if(!userInput.equals("exit")) {
+                    bufOutput.write(userInput + "\n");
+                }
+
+                // bufOutput.newLine();
+            } while(!userInput.equals("exit")); //cat timp inputul utilizatorului este diferit de "exit"
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+
+    }
+
+    private static boolean isFinishCondition(String userInput) {
+        return !userInput.equals("exit");
+    }
+}
