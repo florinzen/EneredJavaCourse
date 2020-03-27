545e3e52ee094c63a4ae705b076a93b3451f651b
Andrei Herdes <a.herdes@levi9.com>
2020-03-27 20:09:59 +0200

    Updated example for FileVisitor

--- SandboxEnered/src/module3/examples/ioexamples/nio/FileVisitorExample.java
+++ SandboxEnered/src/module3/examples/ioexamples/nio/FileVisitorExample.java
@@ -1,9 +1,8 @@
 package module3.examples.ioexamples.nio;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileVisitorExample {
     public static void main(String[] args) {
@@ -11,6 +10,33 @@ public class FileVisitorExample {
         FileVisitorImpl visitor = new FileVisitorImpl();
         try {
             Files.walkFileTree(startingDir, visitor);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Or we can use an anonymous class!
+        try {
+            Files.walkFileTree(startingDir, new FileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path path, IOException e) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
